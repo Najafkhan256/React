e1b875a58f6cd16686a0ae764ba2c974804bbe99
Najaf <najafkhan6625@gmail.com>
2023-01-11 20:00:59 +0500

    React components and props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
-import Heading from "./Components/Heading";
-import FavFruits from "./Components/FavFruits";
+import Heading from "./components/Heading";
+import Fav from "./components/FavFruits";
 
+const Fruit = ["Mango", "Banana", "Pine apple", "Apple"];
+const Hobbies = ["Speaking", "Reading", "Walking"];
 function App() {
   return (
     <div className="App">
-      <Heading />
-      <FavFruits />
+      <Heading text="My favourite Fruits" />
+      <Fav Fruit={Fruit} />
+      <Heading text="My Hobbies" />
+      <Fav Fruit={Hobbies} />
     </div>
   );
 }

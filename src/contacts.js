67c2de4a588e5a6aa1b4749b15c67f0120421fc8
Najaf <najafkhan6625@gmail.com>
2023-01-12 15:52:45 +0500

    Mapping Components

---
+++ src/contacts.js
@@ -0,0 +1,26 @@
+export const Contacts = [
+  {
+    id: 1,
+    name: "Najaf Khan",
+    img: "https://picsum.photos/100/100",
+    phone: "+123 456 789",
+    email: "example@gmail.com",
+    className: "first-card",
+  },
+  {
+    id: 2,
+    name: "Annus Khan",
+    img: "/images/najafkhan.png",
+    phone: "+123 456 789",
+    email: "example@gmail.com",
+    className: "second-card",
+  },
+  {
+    id: 3,
+    name: "Rayyan Khan",
+    img: "https://picsum.photos/200/200",
+    phone: "+123 456 789",
+    email: "example@gmail.com",
+    className: "third-card",
+  },
+];
